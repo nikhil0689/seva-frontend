e217bbf288346080af751370427759e2b3efc93e
Nikhil Chandrashekar <nikhil0689@gmail.com>
2022-01-31 22:18:20 -0800

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,5 +1,26 @@
-const validEmailRegex = RegExp(
+export const validEmailRegex = RegExp(
     /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
   );
   
-const onlyAlphabets = /^[0-9\b]+$/;
+export const FIRST_NAME="firstname";
+export const LAST_NAME="lastname";
+export const GOTHRA="gothra";
+export const NAKSHATHRA="nakshatra";
+export const PHONE="phone";
+export const EMAIL="email";
+export const STREET="street";
+export const UNIT="unit";
+export const CITY="city";
+export const STATE="state";
+export const ZIP="zip";
+export const WHATSAPP="whatsapp";
+export const WEEKLY_EMAIL="weekly_email";
+export const MAHA_RUDRA="maha_rudra";
+export const FIRST_NAME_CANNOT_BE_EMPTY="First Name cannot be empty";
+export const PHONE_CANNOT_BE_EMPTY="Phone cannot be empty";
+export const EMPTY_STRING="";
+export const INVALID_PHONE_NO="Invalid Phone No.";
+export const INVALID_EMAIL_ID="Invalid Email Id";
+export const EMAIL_CANNOT_BE_EMPTY="Email cannot be empty";
+export const YES="yes"
+export const NO="no"
